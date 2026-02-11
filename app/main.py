d066a8609e082c6ccb6244d16e745cd7fce80430
Kosh_from_Vorlon <koshfromvorlon@gmail.com>
2026-02-11 12:24:08 +0200

    visual done

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 from . import models, database
-import random
 
 app = FastAPI()
 
